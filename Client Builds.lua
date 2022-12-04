b5149cd426d02b8a8b0f4d0441b50c19f9323ffc
Real Radiant <91288974+morphh7@users.noreply.github.com>
2022-12-04 20:42:36 +0100

    Update Client Builds.lua

--- Client Builds.lua
+++ Client Builds.lua
@@ -3,18 +3,3 @@ local Clients = {
     "qhouz",
     "Debug"
 }
-
-local tablelength = function(T)
-    local count = 0
-    for _ in pairs(T) do 
-        count = count + 1 
-    end
-    return count
-end
-
-local function Main()
-    local counting = tablelength(Clients)
-    return counting
-end
-
-return Main()
