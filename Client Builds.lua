4e8b6014afc1088307d0f4dca0685b67a202bdad
Real Radiant <91288974+morphh7@users.noreply.github.com>
2022-12-04 20:08:31 +0100

    Update and rename Client Builds to Client Builds.lua

---
+++ Client Builds.lua
@@ -0,0 +1,26 @@
+local Clients = {}
+local username = common.get_username()
+Clients["7morphh"] = true
+Clients["qhouz"] = true
+Clients["Debug"] = true
+
+local function sendtrue()
+    if Clients[username] == true then
+        return true
+    else
+        return false
+    end
+end
+
+local function tablelength(T)
+    local count = 0
+    for _ in pairs(T) do 
+        count = count + 1 
+    end
+    return count
+end
+
+return{(
+valid = sendtrue()
+Build = tablelength(Clients)
+)}
