2a766f6b7137d075b37e8de6c049b3af5e10a349
Real Radiant <91288974+morphh7@users.noreply.github.com>
2022-12-11 19:23:23 +0100

    Update Client Builds.lua

--- Client Builds.lua
+++ Client Builds.lua
@@ -3,12 +3,4 @@ client["7morphh"] = true
 client["qhouz"] = true
 client["Debug"] = true
 
-local table = {
-    list = (function()
-        local count = 0
-            for _ in pairs(client) do 
-                count = count + 1 
-            end
-        return count
-    end)()
-}
+return
